da457f961b389a0c37d7b8d414c901e989782941
YJammak <yjk@yjk.name>
2025-09-15 14:16:21 +0800

    Adding a simple test

---
+++ src/Simplify.ReactiveUI.Test/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace Simplify.ReactiveUI.Test;
+
+[SplatRegister]
+public class Address
+{
+    public required string AddressLine1 { get; init; }
+    public required string AddressLine2 { get; init; }
+    public required string City { get; init; }
+    public required string State { get; init; }
+    public required string PostalCode { get; init; }
+    public required string Country { get; init; }
+}
